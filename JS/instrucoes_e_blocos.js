f4dc68a0db882937e19dcd224064343a1a5763b2
Adryel0713 <clauber.adriel@gmail.com>
2025-05-31 13:34:18 -0300

    instrucoes_e_blocos.js

---
+++ JS/instrucoes_e_blocos.js
@@ -0,0 +1,17 @@
+// Instruções
+
+console.log("Receita de Bolo")
+console.log("Pegue a farinha e o trigo")
+console.log("E dê para um padeiro")
+
+// Bloco de códigos
+
+{
+  console.log("e")
+  {
+    console.log("pronto")
+    {
+      console.log("O bolo ta pronto")
+    }
+  }
+}
