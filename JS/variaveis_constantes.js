19b0bca065a063441c8a7f218481feb4debffde7
Adryel0713 <clauber.adriel@gmail.com>
2025-06-01 17:16:59 -0300

    variaveis_constantes.js

---
+++ JS/variaveis_constantes.js
@@ -0,0 +1,8 @@
+let idade = 66;
+
+var minimo = 18;
+var senhor = 60;
+
+let calculo = idade>minimo && idade<senhor ? "É maior de idade e menor que 60 anos" : idade > 60 ? "idade maior que 60" : "menor de idade"; 
+
+calculo;
