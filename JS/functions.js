a58eb425683f79d51edce7bf0b3af546c96f16db
Adryel0713 <clauber.adriel@gmail.com>
2025-06-01 19:05:12 -0300

    functions.js

---
+++ JS/functions.js
@@ -0,0 +1,17 @@
+let soma = 0;
+
+setSoma(2,2);
+
+function setSoma(valor1,valor2){
+  soma = valor1 + valor2;
+}
+
+function getSoma(){
+  return soma;
+}
+
+
+console.log(getSoma());
+
+////////////////////////////////////
+
