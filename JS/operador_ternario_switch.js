326a7fde33ed0e2eebba11b5d3d4cd70308fa27f
Adryel0713 <clauber.adriel@gmail.com>
2025-06-01 18:01:57 -0300

    operador_ternario_switch.js

---
+++ JS/operador_ternario_switch.js
@@ -0,0 +1,20 @@
+/*
+var teste = 100 == 100 ? "100 é igual 100" : "20 é diferente 100";
+var teste2 = 20 != 100 ? "20 é diferente de 100" : 20 < 100 ? "20 é menor que 100" : "nem um dos 4";
+
+var number = 4;
+
+switch(number){
+  case 1:
+    console.log("caso 1")
+    break;
+  case 2:
+    console.log("caso 2")
+    break;
+  case 3:
+    console.log("caso 3")
+    break;
+  default:
+    console.log("nem um deles")
+}
+*/
