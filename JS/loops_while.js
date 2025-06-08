d584d40bb58230b25e9ef9c5c8d8e1e104bc56b7
Adryel0713 <clauber.adriel@gmail.com>
2025-06-08 08:43:17 -0300

    loops_while.js

---
+++ JS/loops_while.js
@@ -0,0 +1,18 @@
+console.clear();
+
+let nomes = [
+    "João",
+    "Pedro",
+    "Lucas",
+    "Gabriel",
+    "Thomas"
+]
+
+let totNumb = nomes.length
+let begin = 0;
+while (begin < totNumb) {
+    console.log("Repetindo... " + begin)
+    console.log(nomes[begin])
+    console.log()
+    begin++;
+}
