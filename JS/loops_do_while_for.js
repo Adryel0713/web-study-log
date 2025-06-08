7170a7ac87ce86fd250bd874beaba725edb84a29
Adryel0713 <clauber.adriel@gmail.com>
2025-06-08 08:43:40 -0300

    loops_do_while_for.js

---
+++ JS/loops_do_while_for.js
@@ -0,0 +1,43 @@
+console.clear();
+
+// let nomes = [
+//     "João",
+//     "Pedro",
+//     "Lucas",
+//     "Gabriel",
+//     "Thomas"
+// ]
+
+// let total = nomes.length;
+// let indice = 0;
+// do {
+//     console.log("Indice: " + indice)
+//     console.log(nomes[indice])
+//     indice++;
+// } while (indice < total);
+
+// 
+// let nomes = [
+//     "João",
+//     "Pedro",
+//     "Lucas",
+//     "Gabriel",
+//     "Thomas"
+// ]
+// let total = nomes.length;
+
+// for(let i = 0; i < total; i++){
+//     console.log("Indice: " + i);
+//     console.log(nomes[i]);
+// }
+
+let nomes = [
+    "João",
+    "Pedro",
+    "Lucas",
+    "Gabriel",
+    "Thomas"
+]
+for(indice in nomes){
+    console.log(nomes[indice])
+}
