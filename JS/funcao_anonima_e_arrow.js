a45e7cf7eb74864b0e612678a34ed1086ca25aef
Adryel0713 <clauber.adriel@gmail.com>
2025-06-08 10:34:28 -0300

    funcao_anonima_e_arrow.js

---
+++ JS/funcao_anonima_e_arrow.js
@@ -0,0 +1,30 @@
+console.clear();
+
+// function soma(a,b){
+//     console.log(a+b);
+// }
+// soma(2,3);
+
+////////////////////////////////////
+
+// let soma = function(a,b){
+//     console.log(a+b);
+// }
+// soma(2,3);
+
+////////////////////////////////////
+
+// let soma = (a,b) => {
+//     console.log(a+b);
+// }
+// soma(1,2);
+
+////////////////////////////////////
+
+// let soma = (a,b) => console.log(a+b);
+// soma(1,5);
+
+////////////////////////////////////
+
+// let nome = n => console.log("nome: " + n);
+// nome("Clauber");
