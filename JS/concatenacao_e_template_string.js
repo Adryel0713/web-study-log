2a140b2f2415a635eb6be194f8325897bdc6f624
Adryel0713 <clauber.adriel@gmail.com>
2025-06-08 08:05:05 -0300

    concatenacao_e_template_string.js

---
+++ JS/concatenacao_e_template_string.js
@@ -0,0 +1,10 @@
+console.clear();
+
+const ana = "ana"
+let numero = 20;
+console.log(ana + numero)
+
+console.log("A " + ana + " tem " + numero)
+
+const texto = `Olá ${ana}, Tudo bom??
+Você teria ${numero} reais para me emprestar por gentileza?`
