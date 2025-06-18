c10bda2cee5ce4d2b293ac8d44a2221a500f225c
Adryel0713 <clauber.adriel@gmail.com>
2025-06-18 16:15:07 -0300

    array_metodos.js

---
+++ JS/array_metodos.js
@@ -0,0 +1,15 @@
+let array = ["Adryel","Araujo","de"]
+
+// array.push("Souza") //adiciona ultimo
+// array.pop() //remove ultimo
+
+// array.unshift("Clauber") //adiciona primeiro
+// array.shift() //remove primeiro
+
+// const novo = array.splice(0,1,"Clauber","Adryel") // ele tira é coloca no lugar
+
+//const novo = array.slice(0,2); // só remove
+
+let text = array.join()
+let arrayTexto = text.split(",")
+console.log(arrayTexto);
