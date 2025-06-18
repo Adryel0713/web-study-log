5bc1f5d9d879546ebc8cdf45d6e4494738f3d11d
Adryel0713 <clauber.adriel@gmail.com>
2025-06-18 13:42:42 -0300

    math.js

---
+++ JS/math.js
@@ -0,0 +1,11 @@
+num1 = 10.4;
+
+
+console.log(Math.floor(num1));
+console.log(Math.ceil(num1));
+console.log(Math.round(num1));
+console.log(Math.max(num1, 20));
+console.log(Math.min(num1, 20));
+console.log(Math.random() * 100);
+console.log(Math.pow(2,10));
+console.log(Math.sqrt(144));
